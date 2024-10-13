db245ee6637db2661f62f0b356cf3edf872ed888
Muhammad Asif Ansari <muhammadasifansari101@gmail.com>
2024-10-13 09:57:52 +0500

    Javascript assignment
    Javascript assignment

---
+++ app.js
@@ -0,0 +1,26 @@
+// Question No 01 :
+// var player1 = prompt("Enter a Name");
+// var player2 = prompt("Enter a Name");
+// var num = prompt("Enter a Number");
+
+// var num1 = Math.floor(Math.random(num1)*2);
+// console.log(num1);
+// if(num1 === 0){
+//     console.log(player1 + "It's value print Head");
+// }
+// else{
+//     console.log(player2 + "It's value print Tail");   
+// }
+
+// Question no 02:
+// var userInput = parseInt(prompt("Enter a number between 1 to 6: "), 10);
+// var randomNumber = Math.floor((Math.random()*6)+1);
+// if(userInput < 1 || userInput > 6 || isNaN(userInput)){
+//     alert("Invalid input! Please enter a number between 1 and 6.");
+// }
+// else if(userInput === randomNumber){
+// console.log("Congratulations! You guessed correctly.");
+// }
+// else{
+//     console.log("Sorry,you guessed " + userInput + " incorrectly. The Correct number was: " + randomNumber);
+// }
